b9353cfd37a3f3054d9a117d3f9001237e0f9cdb
setavicente <vseta07@gmail.com>
2025-05-25 17:15:38 -0300

    feat: añado comentarios a animation

--- workspace_lpc845/01_animation/source/01_animation.c
+++ workspace_lpc845/01_animation/source/01_animation.c
@@ -16,14 +16,14 @@
 #include "clock_config.h"
 #include "fsl_debug_console.h"
 /* TODO: insert other include files here. */
-#define SEG_A GPIO, 0 , 11
-#define SEG_F GPIO, 0 , 13
-#define SEG_E GPIO, 0 , 0
-#define SEG_D GPIO, 0 , 14
-#define SEG_C GPIO, 0 , 6
-#define SEG_B GPIO, 0 , 10
-#define A1 GPIO,0,8
-#define USER GPIO,0,4
+#define SEG_A GPIO, 0 , 11  //defino base, puerto y num de GPIO
+#define SEG_F GPIO, 0 , 13	//defino base, puerto y num de GPIO
+#define SEG_E GPIO, 0 , 0	//defino base, puerto y num de GPIO
+#define SEG_D GPIO, 0 , 14	//defino base, puerto y num de GPIO
+#define SEG_C GPIO, 0 , 6	//defino base, puerto y num de GPIO
+#define SEG_B GPIO, 0 , 10	//defino base, puerto y num de GPIO
+#define A1 GPIO,0,8			//defino base, puerto y num de GPIO
+#define USER GPIO,0,4		//defino base, puerto y num de GPIO
 /* TODO: insert other definitions and declarations here. */
 
 /*
@@ -31,14 +31,15 @@
  */
 int main(void) {
 
-	GPIO_PortInit(GPIO, 0);///base y puerto
+	GPIO_PortInit(GPIO, 0);///inicializo base y puerto
 
 	gpio_pin_config_t out_config={ .outputLogic = 1, .pinDirection =kGPIO_DigitalOutput};///outconfig para settear como salida el pin
-	gpio_pin_config_t in_config={ .pinDirection =kGPIO_DigitalInput};
+	gpio_pin_config_t in_config={ .pinDirection =kGPIO_DigitalInput};//inconfig para settear como entrada un pin
 
-	GPIO_PinInit(A1 , &out_config);
+	
+	GPIO_PinInit(A1 , &out_config);//Puntero a la estructura del enum de los pines
 
-	GPIO_PinInit(USER , &in_config);
+	GPIO_PinInit(USER , &in_config);//Puntero a la estructura del enum de los pines
 
 
 	GPIO_PinInit(SEG_B , &out_config);//Puntero a la estructura del enum de los pines
@@ -54,29 +55,29 @@ int main(void) {
     while(1) {
     	int boton = GPIO_PinRead(USER);
     	if(boton==0){
-    	GPIO_PinWrite(A1, 0);
+    	GPIO_PinWrite(A1, 0); //habilito el display 1 
 
-    	GPIO_PinWrite(SEG_B, 0);//base, puerto, pin
-    	for ( uint32_t i= 0; i < 50000; i++);
+    	GPIO_PinWrite(SEG_B, 0);
+    	for ( uint32_t i= 0; i < 50000; i++);//delay
     	GPIO_PinWrite(SEG_B, 1);
     	GPIO_PinWrite(SEG_F, 0);
-    	for ( uint32_t i= 0; i < 50000; i++);
+    	for ( uint32_t i= 0; i < 50000; i++);//delay
     	GPIO_PinWrite(SEG_F, 1);
     	GPIO_PinWrite(SEG_E, 0);
-    	for ( uint32_t i= 0; i < 50000; i++);
+    	for ( uint32_t i= 0; i < 50000; i++);//delay
     	GPIO_PinWrite(SEG_E, 1);
     	GPIO_PinWrite(SEG_D, 0);
-    	for ( uint32_t i= 0; i < 50000; i++);
+    	for ( uint32_t i= 0; i < 50000; i++);//delay
     	GPIO_PinWrite(SEG_D, 1);
     	GPIO_PinWrite(SEG_C, 0);
-    	for ( uint32_t i= 0; i < 50000; i++);
+    	for ( uint32_t i= 0; i < 50000; i++);//delay
     	GPIO_PinWrite(SEG_C, 1);
     	GPIO_PinWrite(SEG_A, 0);
-    	for ( uint32_t i= 0; i < 50000; i++);
+    	for ( uint32_t i= 0; i < 50000; i++);//delay
     	GPIO_PinWrite(SEG_A, 1);
     	}
     	else {
-    	GPIO_PinWrite(A1, 1);
+    	GPIO_PinWrite(A1, 1);//desactivo el display 1
     	}
     }
    return 0 ;
