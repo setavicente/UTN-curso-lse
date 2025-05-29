a409d3e36686a25f22bb870daa7b1ecaf5a33167
setavicente <vseta07@gmail.com>
2025-05-29 16:35:46 -0300

    ...¿?

--- workspace_lpc845/02_custom_blinky_pot/source/02_custom_blinky_pot.c
+++ workspace_lpc845/02_custom_blinky_pot/source/02_custom_blinky_pot.c
@@ -9,10 +9,13 @@
 // Pin para el LED azul
 #define LED_BLUE	1
 
-/*
- * @brief   Application entry point.
- */
+
+
 int main(void) {
+
+	static uint16_t i200ms = 0;
+	static uint16_t i2000ms = 0;
+
 
 	// Inicializacion LED
 	BOARD_BootClockFRO24M();
@@ -61,6 +64,7 @@ int main(void) {
 		.enableSyncBypass = false,								// Sin bypass de trigger
 		.interruptMode = kADC_InterruptForEachConversion		// Interrupciones para cada conversion (ahora no hay interrupcion ya q estamos en polling, puedo hacer que la interrupcion sea x cada seq. o por cada canal)
 	};
+
 	// Configuro y habilito secuencia A
 	ADC_SetConvSeqAConfig(ADC0, &adc_sequence);
 	ADC_EnableConvSeqA(ADC0, true);
@@ -68,6 +72,53 @@ int main(void) {
 
 	// Configuro SysTick para 1 ms (inversa de 1000)
 	SysTick_Config(SystemCoreClock / 1000);//por lo que divida aca, indica cada cuanto tiempo tendre una interrupcion
+
+    while(1) {
+
+
+    	// Informacion (channel, valor, etc) de la lectura de adc la guardo en adc_info
+    	adc_result_info_t adc_info;
+    	// Inicio conversion
+    	ADC_DoSoftwareTriggerConvSeqA(ADC0);
+    	// Espero a terminar la conversion
+    	while(!ADC_GetChannelConversionResult(ADC0, ADC_POT_CH, &adc_info));//esto define el polling, espero constantemente en esta linea hasta realizar la conversion. una vez hecha la conversion el código sigue.
+
+    	if (adc_info.result <= 2047){
+
+    		// Verifico si el SysTick se disparo 200 veces
+    		if(i200ms == 200) {
+    			// Reinicio el contador
+    			i200ms = 0;
+    			// Conmuto el LED
+    			GPIO_PinWrite(GPIO, 1, LED_BLUE, !GPIO_PinRead(GPIO, 1, LED_BLUE));
+    		}
+    	}
+
+
+		if (adc_info.result >= 2047){ //adc_info.result tiene el valor de adc
+
+			// Verifico si el SysTick se disparo 2000 veces
+			if(i2000ms == 2000) {
+				// Reinicio el contador
+				i2000ms = 0;
+				// Conmuto el LED
+				GPIO_PinWrite(GPIO, 1, LED_BLUE, !GPIO_PinRead(GPIO, 1, LED_BLUE));
+			}
+
+
+
+    	}
+
+
+		// Muestro el valor de adc
+    	PRINTF(
+			"El resultado del canal %ld en el PIO0_7 dio %d\n",
+			adc_info.channelNumber, //adc_info.channelNumber tiene el valor de canal del adc usado
+			adc_info.result); //adc_info.result tiene el valor de adc
+
+    return 0;
+
+}
 }
 
 
@@ -82,51 +133,6 @@ void SysTick_Handler(void) {
 	i200ms++;
 	i2000ms++;
 
-    while(1) {
-
-
-    	// Resultado de conversion lo guardo en adc_info
-    	adc_result_info_t adc_info;
-    	// Inicio conversion
-    	ADC_DoSoftwareTriggerConvSeqA(ADC0);
-    	// Espero a terminar la conversion
-    	while(!ADC_GetChannelConversionResult(ADC0, ADC_POT_CH, &adc_info));//esto define el polling, espero constantemente en esta linea hasta realizar la conversion. una vez hecha la conversion el código sigue.
-
-    	if (adc_info<=2047){
-
-    		// Verifico si el SysTick se disparo 200 veces
-    		if(i200ms == 200) {
-    			// Reinicio el contador
-    			i = 0;
-    			// Conmuto el LED
-    			GPIO_PinWrite(GPIO, 1, LED_BLUE, !GPIO_PinRead(GPIO, 1, LED_BLUE));
-    		}
-    	}
-
-		if (adc_info>=2047){
-
-			// Verifico si el SysTick se disparo 2000 veces
-			if(i2000ms == 2000) {
-				// Reinicio el contador
-				i = 0;
-				// Conmuto el LED
-				GPIO_PinWrite(GPIO, 1, LED_BLUE, !GPIO_PinRead(GPIO, 1, LED_BLUE));
-			}
-
-
-
-    	}
-
-
-		// Muestro resultado del adc
-    	PRINTF(
-			"El resultado del canal %ld en el PIO0_7 dio %d\n",
-			adc_info.channelNumber,
-			adc_info.result); //funcion para exponer el resultado de la conversion
-
-    return 0;
-
-}
 }
 
 
